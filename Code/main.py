681b9a6b0bde6597bb6264e05002d3c8c9907af1
dchbib <dyaa.chbib@gmail.com>
2022-01-09 21:08:01 +0100

    Main program

--- Code/main.py
+++ Code/main.py
@@ -10,25 +10,34 @@ from PipelineStages import pipeline
 pathFile = 'OnlineRetail.xlsx'
 
 #Definition of the DataBase class and his objects 
-DataBase = ManageDataset(pathFile, username='dchbib', password='q7VmFR79HCFkUTKP')
+DataBase = ManageDataset(pathFile, username='User', password='Password')
 
 dataSet = DataBase.prepareFilesToMongoDB()
 
 dbOnServer = DataBase.insertDataToMongoDB(projectName='OnlineRetail', datasetSerie=dataSet)
 
+# The customer spent the most money // CustomerId : 16446 with Amount = 168469.6
+information_lookingFor = "aggre_product"  # Possible values : "aggre_product", "aggre_customer",  "aggre_price", "aggre_invoice"
+pipelines = pipeline(information_lookingFor) 
+groupedata = DataBase.aggregations(dbOnServer, pipelines)  
+
+print(groupedata[1][0])
+
+# The product sold the most // 'PAPER CRAFT , LITTLE BIRDIE'
+information_lookingFor = "aggre_customer" #   
+pipelines = pipeline(information_lookingFor) 
+groupedata = DataBase.aggregations(dbOnServer, pipelines)  
+
+# The average of unit price // avg = 66.8968
+information_lookingFor = "aggre_price" #   
+pipelines = pipeline(information_lookingFor) 
+groupedata = DataBase.aggregations(dbOnServer, pipelines)  
+
 # Groupby invoice and plot the charts 
-information_lookingFor = "" # "aggre_invoice" by default   
+information_lookingFor = "aggre_invoice"
 pipelines = pipeline(information_lookingFor) 
 groupedata = DataBase.aggregations(dbOnServer, pipelines)  
-
-print(groupedata)
 DataBase.graphics(groupedata[1][0])
-
-# To obtain the average of unit price // avg = 3.2370318148
-information_lookingFor = "aggre_price" #  "aggre_customer", "aggre_product", "aggre_price"
-pipelines = pipeline(information_lookingFor) 
-groupedata = DataBase.aggregations(dbOnServer, pipelines)  
-
 
 #To delete data 
 DataBase.drop_db(projectName='OnlineRetail')
